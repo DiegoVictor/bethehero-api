94270b75f49626d14e1aef56172ffddb1d04d028
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-06 14:37:35 -0300

    Improve alias name and make hateoas manually

--- src/app/controllers/IncidentController.js
+++ src/app/controllers/IncidentController.js
@@ -2,11 +2,10 @@ import { notFound, unauthorized } from '@hapi/boom';
 
 import connection from '../../database/connection';
 import paginationLinks from '../helpers/paginationLinks';
-import hateoas from '../helpers/hateoas';
 
 class IncidentController {
   async index(req, res) {
-    const { base_url, resource_url } = req;
+    const { host_url, current_url } = req;
     const { page = 1 } = req.query;
     const limit = 5;
 
@@ -15,77 +14,50 @@ class IncidentController {
       .limit(limit)
       .offset((page - 1) * limit)
       .select(['ngos.*', 'ngos.id as ngo_id', 'incidents.*'])
-      .then((data) => {
-        return data.map((incident) =>
-          hateoas(
-            {
-              id: incident.id,
-              title: incident.title,
-              description: incident.description,
-              value: incident.value,
-              ngo: {
-                id: incident.ngo_id,
-                name: incident.name,
-                email: incident.email,
-                whatsapp: incident.whatsapp,
-                city: incident.city,
-                uf: incident.uf,
-              },
-            },
-            {
-              url: `${resource_url}/:id`,
-              ngo: {
-                url: `${base_url}/v1/ngos/${incident.ngo_id}`,
-              },
-            }
-          )
-        );
-      });
+      .then((data) =>
+        data.map((incident) => ({
+          id: incident.id,
+          title: incident.title,
+          description: incident.description,
+          value: incident.value,
+          ngo: {
+            id: incident.ngo_id,
+            name: incident.name,
+            email: incident.email,
+            whatsapp: incident.whatsapp,
+            city: incident.city,
+            uf: incident.uf,
+            url: `${host_url}/v1/ngos/${incident.ngo_id}`,
+          },
+          url: `${current_url}/${incident.id}`,
+        }))
+      );
 
     const [count] = await connection('incidents').count();
     res.header('X-Total-Count', count['count(*)']);
 
     const pages_total = Math.ceil(count['count(*)'] / limit);
     if (pages_total > 1) {
-      res.links(paginationLinks(page, pages_total, resource_url));
+      res.links(paginationLinks(page, pages_total, current_url));
     }
 
     return res.json(incidents);
   }
 
   async show(req, res) {
-    const { base_url, resource_url } = req;
+    const { host_url, current_url } = req;
     const { id } = req.params;
-    const incident = await connection('incidents')
-      .where('id', id)
-      .first()
-      .then((result) => {
-        if (result) {
-          return {
-            id,
-            title: result.title,
-            description: result.description,
-            value: result.value,
-            ngo: {
-              id: result.ngo_id,
-            },
-          };
-        }
-        return result;
-      });
+    const incident = await connection('incidents').where('id', id).first();
 
     if (!incident) {
       throw notFound('Incident not found', { code: 144 });
     }
 
-    return res.json(
-      hateoas(incident, {
-        url: resource_url,
-        ngo: {
-          url: `${base_url}/v1/ngos/${incident.ngo.id}`,
-        },
-      })
-    );
+    return res.json({
+      ...incident,
+      ngo_url: `${host_url}/v1/ngos/${incident.ngo_id}`,
+      url: current_url,
+    });
   }
 
   async store(req, res) {
