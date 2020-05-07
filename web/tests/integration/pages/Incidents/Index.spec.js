813c94b64e0317f3990ebac39b7923b954578410
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-07 13:52:42 -0300

    Update test with context data

--- web/tests/integration/pages/Incidents/Index.spec.js
+++ web/tests/integration/pages/Incidents/Index.spec.js
@@ -6,6 +6,7 @@ import { createBrowserHistory } from 'history';
 import MockAdapter from 'axios-mock-adapter';
 import faker from 'faker';
 
+import NgoContext from '~/contexts/Ngo';
 import api from '~/services/api';
 import factory from '../../../utils/factory';
 import Index from '~/pages/Incidents/Index';
@@ -15,27 +16,26 @@ jest.mock('react-toastify');
 describe('Incidents/Index', () => {
   const api_mock = new MockAdapter(api);
   const history = createBrowserHistory();
+  const ngo = { id: faker.random.number(), name: faker.name.findName() };
+  const setNgo = jest.fn();
 
   beforeEach(() => {
-    const name = faker.name.findName();
-    const token = faker.random.alphaNumeric(16);
-
-    localStorage.clear();
-    localStorage.setItem('bethehero', JSON.stringify({ name, token }));
     api_mock.reset();
   });
 
   it('should be able to retrieve a list of incidents', async () => {
     const incidents = await factory.attrsMany('Incident', 3);
-    api_mock.onGet('/ngo_incidents').reply(200, incidents);
+    api_mock.onGet(`/ngos/${ngo.id}/incidents`).reply(200, incidents);
 
     let getByText;
     let getByTestId;
     await act(async () => {
       const component = render(
-        <Router history={history}>
-          <Index />
-        </Router>
+        <NgoContext.Provider value={{ ngo, setNgo }}>
+          <Router history={history}>
+            <Index />
+          </Router>
+        </NgoContext.Provider>
       );
 
       getByText = component.getByText;
@@ -62,18 +62,20 @@ describe('Incidents/Index', () => {
 
     const incidents = await factory.attrsMany('Incident', 10);
     api_mock
-      .onGet('/ngo_incidents', { params: { page: 1 } })
+      .onGet(`/ngos/${ngo.id}/incidents`, { params: { page: 1 } })
       .reply(200, incidents.slice(0, 5))
-      .onGet('/ngo_incidents', { params: { page: 2 } })
+      .onGet(`/ngos/${ngo.id}/incidents`, { params: { page: 2 } })
       .reply(200, incidents.slice(-5), { link: '' });
 
     let getByText;
     let getByTestId;
     await act(async () => {
       const component = render(
-        <Router history={history}>
-          <Index />
-        </Router>
+        <NgoContext.Provider value={{ ngo, setNgo }}>
+          <Router history={history}>
+            <Index />
+          </Router>
+        </NgoContext.Provider>
       );
 
       getByText = component.getByText;
@@ -108,7 +110,7 @@ describe('Incidents/Index', () => {
     toast.success = jest.fn();
 
     api_mock
-      .onGet('/ngo_incidents')
+      .onGet(`/ngos/${ngo.id}/incidents`)
       .reply(200, [incident])
       .onDelete(`/incidents/${incident.id}`)
       .reply(200);
@@ -116,9 +118,11 @@ describe('Incidents/Index', () => {
     let getByTestId;
     await act(async () => {
       const component = render(
-        <Router history={history}>
-          <Index />
-        </Router>
+        <NgoContext.Provider value={{ ngo, setNgo }}>
+          <Router history={history}>
+            <Index />
+          </Router>
+        </NgoContext.Provider>
       );
 
       getByTestId = component.getByTestId;
@@ -136,7 +140,7 @@ describe('Incidents/Index', () => {
     toast.error = jest.fn();
 
     api_mock
-      .onGet('/ngo_incidents')
+      .onGet(`/ngos/${ngo.id}/incidents`)
       .reply(200, [incident])
       .onDelete(`/incidents/${incident.id}`)
       .reply(400);
@@ -144,9 +148,11 @@ describe('Incidents/Index', () => {
     let getByTestId;
     await act(async () => {
       const component = render(
-        <Router history={history}>
-          <Index />
-        </Router>
+        <NgoContext.Provider value={{ ngo, setNgo }}>
+          <Router history={history}>
+            <Index />
+          </Router>
+        </NgoContext.Provider>
       );
 
       getByTestId = component.getByTestId;
@@ -162,14 +168,16 @@ describe('Incidents/Index', () => {
   });
 
   it('should be able to logout', async () => {
-    api_mock.onGet('/ngo_incidents').reply(200, []);
+    api_mock.onGet(`/ngos/${ngo.id}/incidents`).reply(200, []);
 
     let getByTestId;
     await act(async () => {
       const component = render(
-        <Router history={history}>
-          <Index />
-        </Router>
+        <NgoContext.Provider value={{ ngo, setNgo }}>
+          <Router history={history}>
+            <Index />
+          </Router>
+        </NgoContext.Provider>
       );
 
       getByTestId = component.getByTestId;
