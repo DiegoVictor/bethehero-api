828562641e134501b3f836a3bba69579ff598d0f
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-27 22:33:11 -0300

    Create docs endpoint

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import helmet from 'helmet';
 import { isBoom } from '@hapi/boom';
+import swagger from 'swagger-ui-express';
 
+import swaggerDocument from './swagger.json';
 import routes from './routes';
 import routeAliases from './app/middlewares/routeAliases';
 
@@ -21,6 +23,7 @@ app.use(
 app.use(express.json());
 app.use(routeAliases);
 
+app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use('/v1/', routes);
 
 app.use(errors());
